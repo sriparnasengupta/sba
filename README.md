1c8caebe66db252d931e84bbd595c7ee6f88bc8d
sriparnasengupta <sriparna.sengupta@cognizant.com>
2019-03-29 04:39:01 -0700

    Report generations, set up documentation

---
+++ README.md
@@ -0,0 +1,42 @@
+# SBA
+
+Online repository for the IIHT Cognizant FSE Project Manager application
+
+Folder Structure
+  - client
+  - codeCoverageReport
+  - database
+  - server
+  - Project_Manager_Setup_Document.docx
+  - Load_Testing_Result
+  - Jenkins_build_report
+
+# Important instructions
+
+> Read the Project_Manager_Setup_Document.docx carefully
+> This contains all the instruction to set up this application
+
+
+
+### Installation
+
+Required Software
+- Visual Studio 2017
+- Visual Studio Code (preferable latest version)
+- SQL Server Management Studio 2017
+- Google Chrome
+- Node JS v 8.12.0
+- GIT BASH and GIT UI (for connecting to the repository)
+
+In the client folder open command prompt and run these commands
+
+```sh
+npm install –g -f @angular/cli
+npm install -f
+```
+
+After the installation to run the angular application run the command
+
+```
+$ npm start
+```
---
+++ server/ProjectManagement/ProjectManager.LoadTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ProjectManager.LoadTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProjectManager.LoadTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("022e38a8-fbc1-47b4-9e45-dd9c75bad956")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
